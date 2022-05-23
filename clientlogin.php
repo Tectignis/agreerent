af414bb68dfba7884285cd84436d612412be0495
whynaidu <naiduvedant@gmail.com>
2022-05-23 14:21:13 +0530

    Chnages

--- clientlogin.php
+++ clientlogin.php
@@ -1,3 +1,34 @@
+<?php
+include('config/config.php');
+session_start();
+if(isset($_POST["login"])){
+	$email=$_POST["email"];
+	$password=$_POST["password"];
+
+	$sql = mysqli_query($conn,"SELECT * FROM agent_details WHERE email='$email'") ;
+	if(mysqli_num_rows($sql)>0){
+		$row=mysqli_fetch_assoc($sql); 
+		$verify=password_verify($password,$row['password']);
+	
+
+		if($verify==1){
+			echo"<script>alert('Login Sucessful'),window.location='index.php';</script>";
+			$_SESSION['email']=$row['email'];
+      $_SESSION['name']=$row['agent_name'];
+      $_SESSION['id']=$row['user_id'];
+      $_SESSION['address']=$row['office_address'];
+		}
+		else{
+			echo"<script>alert('Invalid Password'),window.location='login.php';</script>";
+		}
+	}else{
+		echo"<script>alert('Wrong email'),window.location='login.php';</script>";
+	}
+	}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,9 +56,9 @@
     <div class="card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="index3.html" method="post">
+      <form method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" name="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -35,7 +66,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -45,20 +76,19 @@
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
-              <input type="checkbox" id="remember">
+              <input type="checkbox" name="remember" id="remember">
               <label for="remember">
                 Remember Me
               </label>
             </div>
           </div>
         </div>
+        <div class="social-auth-links text-center mt-2 mb-3">
+        <input type="submit" name="login" value="Login" class="btn btn-block btn-primary">
+      </div>
       </form>
 
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-           Sign in 
-        </a>
-      </div>
+      
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
