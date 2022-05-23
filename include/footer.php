11ee3b3eaeeebf3a7b9544e115abb388e41e25e3
whynaidu <naiduvedant@gmail.com>
2022-05-23 12:21:36 +0530

    first commit

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+ <footer class="main-footer">
+    <strong>PRODUCT BY <a href="https://tectignis.in">TECTIGNIS IT SOLUTIONS</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0
+    </div>
+  </footer>
