8d01e9e2bba148bdf32036211dbaf5a037827de7
kajal281199 <kajalpatil281199@gmail.com>
2022-06-14 13:56:57 +0530

    c

--- admin/consultantregistration.php
+++ admin/consultantregistration.php
@@ -1,9 +1,9 @@
 <?php  
-session_start();
-if(!isset($_SESSION['id'])) // If session is not set then redirect to Login Page
-{
- header("Location:login.php"); 
-}
+// session_start();
+// if(!isset($_SESSION['id'])) // If session is not set then redirect to Login Page
+// {
+//  header("Location:login.php"); 
+// }
 include("../config/config.php");
 
 
