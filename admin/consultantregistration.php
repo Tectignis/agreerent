850109490f10aa58d903f454958adc16d28fbcc5
kajal281199 <kajalpatil281199@gmail.com>
2022-06-14 13:18:02 +0530

    c

--- admin/consultantregistration.php
+++ admin/consultantregistration.php
@@ -50,6 +50,8 @@ Aashiyana CHS Shop No 05, Sector 11, Plot No 29, Kamothe, Navi Mumbai, Maharasht
    else{
      echo "<script>alert('Something Wrong');</script>";
    }
+ }else{
+    echo "dddd";
  }
 }
 catch(\Exception $e){
