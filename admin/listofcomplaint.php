5556e69792566382473c8518fc5fd4d25dfcd1ff
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-06-16 13:11:30 +0530

    upadate modal in list of complaints

--- admin/listofcomplaint.php
+++ admin/listofcomplaint.php
@@ -37,6 +37,11 @@ if($sql=1){
     <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"> -->
 
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -97,7 +102,7 @@ if($sql=1){
                                                 <th>Date & Time</th>
                                                 <th>Complaint No</th>
                                                 <th>Subject</th>
-                                                
+                                                <th>Status</th>
                                                 <th>Action</th>
                                             </tr>
                                         </thead>
@@ -114,8 +119,13 @@ if($sql=1){
                                                 <td> <?php echo $arr['date'];?></td>
                                                 <td> <?php echo $arr['complaint_code'];?></td>
                                                 <td> <?php echo $arr['subject'];?> </td>
+                                                <td>  <span class="badge badge-danger">Danger</span></td>
                                                 
                                                 <td>
+                                                <a><button
+                                                            type="button" class="btn btn-success btn-rounded btn-icon"
+                                                            onclick="ConfirmDelete()" data-toggle="modal" data-target="#modal-default" style="color: aliceblue"> <i
+                                                                class="fas fa-edit"></i> </button></a>
                                                     <a href="complaintform.php?delid=<?php echo $arr['id']; ?>"><button
                                                             type="button" class="btn btn-danger btn-rounded btn-icon"
                                                             onclick="ConfirmDelete()" style="color: aliceblue"> <i
@@ -139,6 +149,28 @@ if($sql=1){
             </section>
             <!-- /.content -->
         </div>
+        
+      <div class="modal fade" id="modal-default">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Update</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
         <!-- /.content-wrapper -->
         <?php include 'include/footer.php'; ?>
 
