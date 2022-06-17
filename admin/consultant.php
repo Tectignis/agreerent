722a74d63268311d7f34dc6aed24b91492e6c53a
whynaidu <naiduvedant@gmail.com>
2022-06-17 11:27:50 +0530

    c

--- admin/consultant.php
+++ admin/consultant.php
@@ -197,7 +197,8 @@ if(isset($_POST['submit'])){
 
                                             </select>
                                                 </div>
-                                        
+                                                                                <input type="text" name="aid" class="category">
+
                                     </div>
                             </div>
                             <div class="modal-footer justify-content-between">
