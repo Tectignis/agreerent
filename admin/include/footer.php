7a218699b437d18b03499b60cb9d3d0c5e68ebad
AbhijeetTugave <abhijeettugave@gmail.com>
2022-06-20 11:33:24 +0530

    again change footer #11

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,5 +1,5 @@
  <footer class="main-footer">
-    <div class="card-tools" style="margin-left:790px;">
+    <div class="card-tools" style="display:flex;display-direction:row-reverse;">
      <strong>PRODUCT BY <a href="https://tectignis.in">TECTIGNIS IT SOLUTIONS</a>.</strong>
     </div>
   </footer>
