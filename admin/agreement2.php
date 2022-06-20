41cd2d92af7707cdea8cf03a38c7ceb8ad27d6ad
Govind Bavkar <tectignisofficial@gmail.com>
2022-06-20 13:26:14 +0530

    final agreement ready #8

--- admin/agreement2.php
+++ admin/agreement2.php
@@ -390,7 +390,7 @@ $doc=$arr['docno'];
 
 <!-- police noc form -->
 <div class="pagebreak"> 
-<div class="page">
+
     <h2 align="center">भाडेकरु माहिती फॉर्म</h2>
 	<p class="ex1">रजिस्टर नंबर :-</p>
 	<p class="ex1">दिनांक :-</p>
@@ -597,11 +597,11 @@ $doc=$arr['docno'];
 	<h4 align="center">घोषणापत्र</h4>
 	<p align="center">वरील भरुन दिलेली माहिती ही खरी असून त्यामध्ये काही खोटे आढळल्यास भी कायदेशीर कार्यवाहीस पात्र राहिन.</p>
 	<p align="center">टिप:- सदर माहितीचा / फॉर्मचा उपयोग केवळ पोलीसांच्या रेकॉर्डसाठी असून अन्य कोणत्याही कारणासाठी पुरवा म्हणून वापरता येणार नाही. </p>
-</div> 
+
 </div>
     <!-- receipt -->
     <div class="pagebreak">
-        <div class="page">
+  
     <h1 style="text-align: center">Receipt</h1>
 <?php $sql=mysqli_query($conn,"select tenant.fullname as tname, payment.document_no as dno, payment.security_deposit as rent from payment inner join tenant on payment.document_no=tenant.document_no where payment.document_no");
                  
@@ -714,7 +714,7 @@ $doc=$arr['docno'];
 			<br>
 			<div>2.<?php echo $arr['o2'];?></div>
 			<?php if($doc==$fid){echo $arr['property_type']; }else{ echo ' - ' ;} ?>
-</div>
+
 </div>
 </body>
 
