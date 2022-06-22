d73b965598ec8abf760305c2f061126ec90b5e5f
priya8425 <yadavpriya1425@gmail.com>
2022-06-22 11:06:51 +0530

    footer

--- client/newagreement.php
+++ client/newagreement.php
@@ -822,20 +822,14 @@ $query=mysqli_query($conn,"select * from amenities where id='$deleteid'");
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.2.0
-            </div>
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
-        </footer>
+        <?php include 'include/footer.php'; ?>
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
-    </div>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
     <!-- ./wrapper -->
 
     <!-- jQuery -->
