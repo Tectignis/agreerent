ac39bd395e56f6ddda8a57f2ebabab9aa8296c13
keshridivya <dkeshari094@gmail.com>
2022-06-22 16:07:24 +0530

    c

--- admin/gg.php
+++ admin/gg.php
@@ -511,7 +511,18 @@ else{
                                     <div class="collapse multi-collapse row" id="multiCollapseExample1">
                                         <label for="examplepan" class="col-sm-2 ml-1 col-form-label"></label>
                                         <input type="text" class="form-control mt-2 col-lg-6" name="veriotp" id="veriotp" >
-                                        <p style="color:red;font-size:12px;width:100%;margin-left:17%">Enter OTP sent to your registered email id</p>
+                                        <?php
+                                        if(isset($_POST['otp'])){
+                                        $query=mysqli_query($conn,"select * from agent_details where email='$email_no'");
+                                        if(mysqli_num_rows($query)>0){
+                                            echo "Email already Registered";
+                                        }
+                                        else{
+                                           echo '<p style="color:red;font-size:12px;width:100%;margin-left:17%">Enter OTP sent to your registered email id</p>';
+                                        }
+                                    }
+                                        ?>
+                                        
                                      </div>
                                     </div>
                                     <div class="form-group row">
