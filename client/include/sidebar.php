5692089ad6f34322d3675f23e4dbfae0c2d7c891
snehalambavale <snehal7039@gmail.com>
2022-06-22 16:19:12 +0530

    redone sidebar client

--- client/include/sidebar.php
+++ client/include/sidebar.php
@@ -17,7 +17,7 @@
           <!-- Sidebar user panel (optional) -->
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
               <div class="image">
-                  <img src="dist/img/agent_photo/<?php echo $row['image']; ?>" class="img-circle elevation-2"
+                  <img src="../../admin/dist/img/agent_photo/<?php echo $row['image']; ?>" class="img-circle elevation-2"
                       alt="User Image">
               </div>
               <div class="info">
