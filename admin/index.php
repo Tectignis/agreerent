5fe80ea6e875760dadc23033467bc7d6c6fd7d0a
sarveshhiwale1603 <93575827+sarveshhiwale1603@users.noreply.github.com>
2022-07-20 16:14:41 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -295,9 +295,9 @@ Sales
 <div class="card-body">
 <div class="tab-content p-0">
 
-<div class="chart tab-pane invisible" id="revenue-chart" style="position: relative; height: 300px;">
+<!-- <div class="chart tab-pane invisible" id="revenue-chart" style="position: relative; height: 300px;">
 <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
-</div>
+</div> -->
 <div class="chart tab-pane active" id="sales-chart" style="position: relative; height: 300px;">
 <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
 </div>
