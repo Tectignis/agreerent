deeabaafd79f5fdb035df472d26affa8169b07e6
sarveshhiwale1603 <93575827+sarveshhiwale1603@users.noreply.github.com>
2022-07-20 15:31:38 +0530

    ss

--- admin/index.php
+++ admin/index.php
@@ -298,7 +298,7 @@ Sales
 <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
 <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
 </div>
-<div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+<div class="chart tab-pane invisible" id="sales-chart" style="position: relative; height: 300px;">
 <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
 </div>
 </div>
