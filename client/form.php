f2ce4226238099009d05a80af2cea06715420efd
snehalambavale <snehal7039@gmail.com>
2022-09-08 16:11:54 +0530

    modal

--- client/form.php
+++ client/form.php
@@ -427,7 +427,7 @@ if(isset($_POST['dnkid'])){
             </div></form>';
 }
 if(isset($_POST['dnkidno'])){
-	$sql=mysqli_query($conn,"SELECT * FROM ticket where id='".$_POST['dnkidno']."'");
+	$sql=mysqli_query($conn,"SELECT * FROM `ticket` WHERE id='".$_POST['dnkidno']."'");
 	$arr=mysqli_fetch_array($sql);
 	echo '<div class="modal-header">
               <h4 class="modal-title">'.$arr['complaint_code'].'</h4>
@@ -611,4 +611,49 @@ if(isset($_POST['famidImage'])){
     echo 0;
   }
 }
+
+
+if(isset($_POST['dnkid1'])){
+	$sql=mysqli_query($conn,"SELECT * FROM `property` WHERE id='".$_POST['dnkid1']."'");
+	$arr=mysqli_fetch_array($sql);
+	echo '<div class="modal-header">
+              <h4 class="modal-title">'.$arr['user_id'].'</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+			<form method="post" action="form.php">
+            <div class="modal-body body1">
+                <div class="row">   
+                <div class="col-4">
+                <b> Property Type :</b><br>
+                </div>
+                <div class="col-8">
+                <p> '.$arr['property_for'].' </p>
+                </div>
+                </div>
+
+                <div class="row">   
+                 <div class="col-4">
+                <b> Client Name :</b><br>
+                </div>
+                <div class="col-8">
+                <p> '.$arr['client_name'].' </p>
+                </div>
+                </div>
+
+                <div class="row">   
+                 <div class="col-4">
+                <b> Mobile Number :</b><br>
+                </div>
+                <div class="col-8">
+                <p>'.$arr['mobile_no'].'</p>
+                </div>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div></form>';
+}
+
 ?>
